1fb11ee247ff1b07ffb6acaa7f436a4c94dd982b
peaceboii <kumaravelu2003@gmail.com>
2025-10-24 16:31:38 +0500

    "update"

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 db = SQLAlchemy()
@@ -14,13 +13,16 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    DB_PATH = os.path.join(BASE_DIR, "app.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    
+
+
+    app.config["SECRET_KEY"] = "supersecretkey123"  # change this in production
+   
     login_manager = LoginManager()
     login_manager.init_app(app)
 
